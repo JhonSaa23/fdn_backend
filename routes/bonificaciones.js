da47ca88781a70c5d9a1a9a012885d7f10a14d17
Jhon Saavedra <saavedrafenando879@gmail.com>
2025-07-08 16:45:37 -0500

    KELLY

--- routes/bonificaciones.js
+++ routes/bonificaciones.js
@@ -7,17 +7,34 @@ router.get('/listar', async (req, res) => {
     try {
         const query = `
             SELECT 
-                b.Codproducto, 
-                b.Factor, 
-                b.CodBoni, 
-                b.Cantidad,
-                p.Stock AS StockProducto,
-                p.Nombre AS NombreProducto,
-                pb.Stock AS StockBonificacion,
-                pb.Nombre AS NombreBonificacion
-            FROM Bonificaciones b
-            LEFT JOIN Productos p ON b.Codproducto = p.CodPro
-            LEFT JOIN Productos pb ON b.CodBoni = pb.CodPro
+    b.Codproducto, 
+    b.Factor, 
+    b.CodBoni, 
+    b.Cantidad,
+    p.Stock               AS StockProducto,
+    p.Nombre              AS NombreProducto,
+    pb.Stock              AS StockBonificacion,
+    pb.Nombre             AS NombreBonificacion,
+
+    -- 1) Paquetes completos que puedes armar
+    CAST(p.Stock AS INT) / CAST(b.Factor AS INT) 
+      AS Paquetes,
+
+    -- 2) Cuántos productos de regalo debes comprar para agotar tu stock normal
+    CASE
+      WHEN 
+        (CAST(p.Stock AS INT) / CAST(b.Factor AS INT)) * b.Cantidad
+        > CAST(pb.Stock AS INT)
+      THEN 
+        (CAST(p.Stock AS INT) / CAST(b.Factor AS INT)) * b.Cantidad
+        - CAST(pb.Stock AS INT)
+      ELSE 0
+    END 
+      AS BonosAComprar
+
+FROM Bonificaciones b
+LEFT JOIN Productos p  ON b.Codproducto = p.CodPro
+LEFT JOIN Productos pb ON b.CodBoni     = pb.CodPro;
         `;
         const result = await executeQuery(query);
         res.json({ success: true, data: result.recordset });
